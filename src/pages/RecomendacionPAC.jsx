9a49e64fdc246d0874beaa76034be288c29dd87c
Servaf <laboratoriosdeprocesos@servaf.com>
2026-04-17 11:06:07 -0500

    Conectar frontend con backend de Render

--- src/pages/RecomendacionPAC.jsx
+++ src/pages/RecomendacionPAC.jsx
@@ -25,7 +25,7 @@ function RecomendacionPAC({ setVista }) {
     try {
       setCargando(true);
 
-      const response = await fetch("http://127.0.0.1:8000/recomendar", {
+      const response = await fetch("https://ptap-backend.onrender.com/recomendar", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
